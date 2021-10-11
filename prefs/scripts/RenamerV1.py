1a44a103632f43e9e5f9b07e920b9a28e8f73bf2
Alexsandr Pototskyi <alexsander.potockey@gmail.com>
2021-10-11 18:03:45 +0300

    basic name validation

--- prefs/scripts/RenamerV1.py
+++ prefs/scripts/RenamerV1.py
@@ -1,19 +1,41 @@
 import maya.cmds as cmds
 
-# patterns loaded from Json
-match_obj = ["Intern", "Break", "Apple"]
+# read external configuration file
+def read_config_file():
+	return ["Intern", "Break", "Apple"]
 
-# get current scene objects
-current_scene_objects = cmds.ls(geometry=True)
+def validate_scene_objects():
+	# reading configuration 
+	configuration = read_config_file()
+		
+	# get current scene objects
+	current_scene_objects = cmds.ls(geometry=True)
+	
+	# shape_suffix to remove from shape node
+	shape_suffix = "Shape"
+	
+	# current geometry list
+	filtred_objects = []
+	
+	# removing suffix
+	for scene_object in current_scene_objects:
+		filtred_objects.append(scene_object.replace(shape_suffix, ''))
+	
+	# validating
+	validate(filtred_objects, configuration)
 
-# shape_suffix to remove from shape node
-shape_suffix = "Shape"
 
-# current geometry list
-current_geometry = []
-
-# removing suffix
-for scene_object in current_scene_objects:
-	current_geometry.append(scene_object.replace(shape_suffix, ''))
-
-print current_geometry
+def validate(scene_objects, configuration):
+	not_founded_objects = []
+	
+	for c in configuration:
+		if c not in scene_objects:
+			not_founded_objects.append(c)
+	
+	if len(not_founded_objects) > 0:
+		print "Not Founded Objects in scene"
+		for n in not_founded_objects:
+			print n
+			
+			
+validate_scene_objects()
